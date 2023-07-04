f8695142362390bd72b2b69a1057371824c3ad86
vubinhminh542 <108045166+vubinhminh542@users.noreply.github.com>
2023-07-05 02:31:04 +0700

    Minh price.js

---
+++ frontend/price.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function ProductList() {
+  const [products, setProducts] = useState([
+    { id: 1, name: 'Product 1', price: 10 },
+    { id: 2, name: 'Product 2', price: 15 },
+    { id: 3, name: 'Product 3', price: 20 },
+  ]);
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductList;
